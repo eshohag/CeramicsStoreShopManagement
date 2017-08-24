9aaee59b95d33c725995bf54352abdb3d7e1293a
MD. Shohag Mia <mdshohagmia90@outlook.com>
2017-08-24 20:41:02 +0600

    Add project files.

---
+++ CeramicsStoreShopManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CeramicsStoreShopManagement.Startup))]
+namespace CeramicsStoreShopManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
